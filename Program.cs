41997864d43586d2d6323c92bd4bf5ed26bb0035
Zhongming Chen <ZhongmingC@asos.com>
2018-06-27 18:30:16 +0100

    resolved first cube

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace CryptoCube
@@ -8,116 +7,167 @@ namespace CryptoCube
     {
         static void Main(string[] args)
         {
-            var encrypted = Encrypt(args[0], rotation);
-
-            Console.Write(encrypted);
+            EncryptAndPrint("SoloLearn", "0:R,1:R");
         }
 
-        private static string Encrypt(string input, string rotation)
+        private static void EncryptAndPrint(string input, string rotation)
         {
             var numberOfCubeRequired = input.Length / 8;
-            var cubes = new List<Cube>();
+            var rotationSet = ParseRotations(rotation);
+            var buffer = new char[8];
 
-            for (int i = 0; i < numberOfCubeRequired; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                var vertices = input.Skip(i * 8).Take(8).Select((c, p) => new Vertice(p, c));
-                var cube = new Cube(vertices);
+                var chr = input[i];
+                var vertice = new Vertice(chr, i % 8);
 
-                cube.Rotate();
-                cube.Print();
-                cubes.Add(cube);
+                rotationSet[i / 8].ApplyRotationsInSequence(vertice);
+
+                buffer[vertice.Position] = vertice.Data;
+                
+                if (i % 8 == 7)
+                {
+                    Console.Write(buffer);
+                    buffer = new char[8];
+                }
+            }
+
+            Console.Write(buffer);
+            Console.WriteLine();
+        }
+
+        private static RotationSequence[] ParseRotations(string rotationCommand)
+        {
+            var rotations = rotationCommand
+                .Split(",")
+                .Select(r => new RotationSequence(r));
+
+            return rotations.ToArray();
+        }
+    }
+
+    internal class RotationSequence
+    {
+        private readonly IRotation[] rotations;
+
+        public RotationSequence(string rotationScript)
+        {
+            this.rotations = rotationScript.Split(":").Skip(1).Select(r =>
+            {
+                switch (r)
+                {
+                    case "U": return new RotateUp() as IRotation;
+                    case "D": return new RotateDown() as IRotation;
+                    case "L": return new RotateLeft() as IRotation;
+                    case "R": return new RotateRight() as IRotation;
+                    default:
+                        throw new ArgumentException($"Invalid rotation found {r}");
+                }
+            }).ToArray();
+        }
+
+        internal void ApplyRotationsInSequence(Vertice vertice)
+        {
+            foreach (var rotation in rotations)
+            {
+                rotation.Rotate(vertice);
             }
         }
     }
 
-    class Cube
+    internal interface IRotation
     {
-        private readonly IEnumerable<Vertice> vertices;
-        
-        public Cube(IEnumerable<Vertice> vertices)
+        void Rotate(Vertice vertice);
+    }
+
+    class RotateUp : IRotation
+    {
+        public void Rotate(Vertice vertice)
         {
-            this.vertices = vertices;
+            var position = vertice.Position;
+
+            if (position == 0 || position == 6)
+                vertice.Position++;
+            else if (position == 1 || position == 2)
+                vertice.Position += 4;
+            else if (position == 3 || position == 5)
+                vertice.Position--;
+            else if (position == 4 || position == 7)
+                vertice.Position -= 4;
         }
+    }
 
-        private Stack<string> Rotation { get; set; }
-
-        public void Rotate()
+    class RotateDown : IRotation
+    {
+        public void Rotate(Vertice vertice)
         {
-            foreach(var rotation in Rotation)
-            {
-                rotator.Rotate(vertices);
-            }
+            var position = vertice.Position;
+
+            if (position == 2 || position == 4)
+                vertice.Position++;
+            else if (position == 0 || position == 3)
+                vertice.Position += 4;
+            else if (position == 1 || position == 7)
+                vertice.Position--;
+            else if (position == 5 || position == 6)
+                vertice.Position -= 4;
+
         }
+    }
 
-        public void Print()
+
+    class RotateLeft : IRotation
+    {
+        public void Rotate(Vertice vertice)
         {
+            var position = vertice.Position;
+
+            if (position == 5)
+                vertice.Position++;
+            else if (position == 4)
+                vertice.Position += 3;
+            else if (position == 3)
+                vertice.Position -= 3;
+            else if (position == 0 || position == 1)
+                vertice.Position += 4;
+            else if (position == 2)
+                vertice.Position--;
+            else if (position == 7 || position == 6)
+                vertice.Position -= 4;
+
+        }
+    }
+
+    class RotateRight : IRotation
+    {
+        public void Rotate(Vertice vertice)
+        {
+            var position = vertice.Position;
+
+            if (position == 5 || position == 4)
+                vertice.Position -= 4;
+            else if (position == 7)
+                vertice.Position -= 3;
+            else if (position == 0)
+                vertice.Position += 3;
+            else if (position == 6)
+                vertice.Position--;
+            else if (position == 2 || position == 3)
+                vertice.Position += 4;
+            else if (position == 1)
+                vertice.Position++;
 
         }
     }
 
     class Vertice
     {
-        private readonly char data;
-
-        public Vertice(int position, char data)
+        public Vertice(char data, int position)
         {
-            Position = position;
-            this.data = data;
+            this.Data = data;
+            this.Position = position;
         }
-
-        public int Position { get; private set; }
-        public void Up() {
-            if (Position == 0 || Position == 6)
-                Position++;
-            else if (Position == 1 || Position == 2)
-                Position += 4;
-            else if (Position == 3 || Position == 5)
-                Position--;
-            else if (Position == 4 || Position == 7)
-                Position -= 4;
-        }
-        public void Down()
-        {
-            if (Position == 2 || Position == 4)
-                Position++;
-            else if (Position == 0 || Position == 3)
-                Position += 4;
-            else if (Position == 1 || Position == 7)
-                Position--;
-            else if (Position == 5 || Position == 6)
-                Position -= 4;
-        }
-
-        public void Left()
-        {
-            if (Position == 5)
-                Position++;
-            else if (Position == 4)
-                Position += 3;
-            else if (Position == 3)
-                Position -= 3 ;
-            else if (Position == 0 || Position == 1)
-                Position += 4;
-            else if (Position == 2)
-                Position--;
-            else if (Position == 7 || Position == 6)
-                Position -= 4;
-        }
-
-        public void Right()
-        {
-            if (Position == 5 || Position == 4)
-                Position -= 4;
-            else if (Position == 7)
-                Position -= 3;
-            else if (Position == 0)
-                Position += 3;
-            else if (Position == 6)
-                Position--;
-            else if (Position == 2 || Position == 3)
-                Position += 4;
-            else if (Position == 1)
-                Position++;
-        }
+        public char Data { get; set; }
+        public int Position { get; set; }
     }
 }
